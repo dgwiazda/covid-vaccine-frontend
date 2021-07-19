61b017443e261fa844cab47aa86d798aad23f686
dgwiazda <damiangwiazdaa@wp.pl>
2021-07-19 20:15:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {Router, Switch, Route} from 'react-router-dom';
+
+import VaccineInfoPage from "./views/vaccineInfoPage/VaccineInfoPage";
+import Footer from "./views/homePage/components/Footer";
+import {history} from "./helpers/history"
+
+import styled from 'styled-components'
+import HomePage from "./views/homePage/HomePage";
+import StatisticsPage from "./views/statisticsPage/StatisticsPage";
+import VaccinePointsPage from "./views/vaccinePointsPage/VaccinePointsPage";
+
+const Styles = styled.div`
+
+  #wrapper {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  #page-footer {
+    margin-top: auto;
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Styles>
+            <Router history={history}>
+                <div id="wrapper">
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route path="/vaccines-description" component={VaccineInfoPage}/>
+                        <Route path="/statistics" component={StatisticsPage}/>
+                        <Route path="/vaccine-points" component={VaccinePointsPage}/>
+                    </Switch>
+                    <div id="page-footer">
+                        <Footer/>
+                    </div>
+                </div>
+            </Router>
+        </Styles>
+    )
+        ;
 }
 
 export default App;
